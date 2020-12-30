d93aeb7f380b3eb347fc3d10cea5a066b5280c62
Nik Henri <nik_henri@hotmail.com>
2020-12-30 00:20:26 -0500

    finish

--- gui_pick/app.js
+++ gui_pick/app.js
@@ -1,20 +1,57 @@
-cardImg = ["a3_battle_heimdall_gaze.jpg",
+cardImg = [
+          
            "a3_battle_heimdall_watch.jpg",
            "a3_battle_loki_poison.jpg",
-           "a3_battle_oom_judgment.jpg",
            "a3_battle_thor_ascension.jpg",
+           "a3_battle_oom_judgment.jpg",
            "a3_battle_thor_primacy.jpg",
-           "a3_battle_tyr_judgment.jpg",
            "a3_battle_tyr_rage.jpg",
+           "a3_battle_tyr_judgment.jpg",
+           "a3_battle_heimdall_gaze.jpg",
+
+           "a3_monster_mystic_troll.jpg",
            "a3_monster_soldier_of_hel.jpg",
            "a3_monster_volur_witch.jpg",
+           "a3_monster_frost_giant.jpg",
+           "a3_monster_ymir.jpg",
+           
            "a3_upgrade_eternal_dragons.jpg",
+           "a3_upgrade_master_in_arts.jpg",
+           "a3_upgrade_lord_of_spears.jpg",
+           
            "a3_upgrade_loki_wrath.jpg",
            "a3_upgrade_tyr_wrath.jpg",
+           "a3_upgrade_frigga_domain.jpg",
+           "a3_upgrade_frigga_sacrifice.jpg",
+           "a3_upgrade_odin_throne.jpg",
+           "a3_upgrade_thor_chosen.jpg",
+           "a3_upgrade_thor_conquest.jpg",
+
+           "a3_quest_alfheim.jpg",
+           "a3_quest_jotumheim.jpg",
+           "a3_quest_manheim.jpg",
+           "a3_quest_yggorasil.jpg",
+           "a3_quest_widespread.jpg",
+           "a3_quest_glorious_death.jpg",
           ];
-text_list = ['op', 'op', 'broken', 'need nerf', 'unbalanced']
+text_list = ['op', 'op', 'broken', 'unbalanced']
+
+mp3 = ["duck1.mp3", "duck2.mp3", "duck3.mp3"];
 
 drawAllCard();
+addAudio();
+
+// ------------------------------------------------------------------------
+function addAudio() {
+  for (item of mp3) {
+    audio = document.createElement('audio');
+    src = document.createElement('source');
+    src.src = item;
+    src.type = "audio/mpeg";
+    audio.appendChild(src);
+    document.body.appendChild(audio);
+  }
+}
 
 // ------------------------------------------------------------------------
 function drawAllCard() {
@@ -41,6 +78,8 @@ function event_click_on_card (event) {
   event.target.style.backgroundImage = "";
   event.target.innerText = text_list[getRandomInt(text_list.length)];
   event.target.removeEventListener("click", event_click_on_card);
+  audio = document.querySelectorAll('audio')[getRandomInt(mp3.length)];
+  audio.play();
 }
 
 // ------------------------------------------------------------------------
